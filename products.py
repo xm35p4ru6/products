ad89db66cc314fa7da9a7c7977c84bb076d3860a
xm35p4ru6 <xm35p4ru6@yahoo.com.tw>
2022-09-01 00:08:39 +0800

    存檔版

--- products.py
+++ products.py
@@ -14,5 +14,16 @@ print(products)
 
 # products[0][1] # 大清單[0] 裡面的小清單 [1]
 
+# 'abc' + '123' = 'abc123'
+# 'abc' * 3 = 'abcabcabc'
+# 字串可以做 +, * 不能 -, /
+
 for p in products:
     print(p[0], '的價格是',p[1])
+
+
+# with open('products.csv', 'w', encoding='utf-8') as f:  #遇到亂碼 encoding='utf-8' 使用'utf-8'編碼 來解決亂碼問題
+with open('products.csv', 'w') as f:  # 打開 products.txt, 寫入模式(write) 當作/as f 
+    f.write('商品,價格\n')        
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')    # f.write 真正寫入   \n 換行符號
