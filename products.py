7aa84ea940906d6dc0924c26c6c3cb1e27718b38
xm35p4ru6 <xm35p4ru6@yahoo.com.tw>
2022-09-01 17:08:07 +0800

    檢查檔案功能

--- products.py
+++ products.py
@@ -1,12 +1,21 @@
-# 讀取檔案
+import os
+
 products = []
-with open('products.csv', 'r') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue  
-        name, price = line.strip().split(',') 
-        products.append([name, price])
-print(products)
+
+# 檢查檔案
+if os.path.isfile('products.csv'):
+    print('找到檔案了!!!!')
+
+    # 讀取檔案
+    with open('products.csv', 'r') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue  
+            name, price = line.strip().split(',') 
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案......')
 
 # 讓使用者輸入
 while True:
