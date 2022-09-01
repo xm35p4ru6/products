11775cd9277ea49be4579fae883538fd637867e5
xm35p4ru6 <xm35p4ru6@yahoo.com.tw>
2022-09-01 16:40:19 +0800

    完成版

--- products.py
+++ products.py
@@ -1,29 +1,28 @@
+# 讀取檔案
 products = []
+with open('products.csv', 'r') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue  
+        name, price = line.strip().split(',') 
+        products.append([name, price])
+print(products)
+
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
         break
     price = input('請輸入商品價格: ')
-    # 1 p = []
-    # 2 p.append(name)
-    # 3 p.append(price)
-    # 4 p = [name, price] # 1, 2, 3,簡寫
-    # 5 products.append(p)
-    products.append([name, price]) # 4, 5 簡寫 
+    products.append([name, price])  
 print(products)
 
-# products[0][1] # 大清單[0] 裡面的小清單 [1]
-
-# 'abc' + '123' = 'abc123'
-# 'abc' * 3 = 'abcabcabc'
-# 字串可以做 +, * 不能 -, /
-
+# 印出所有購買紀錄
 for p in products:
     print(p[0], '的價格是',p[1])
 
-
-# with open('products.csv', 'w', encoding='utf-8') as f:  #遇到亂碼 encoding='utf-8' 使用'utf-8'編碼 來解決亂碼問題
-with open('products.csv', 'w') as f:  # 打開 products.txt, 寫入模式(write) 當作/as f 
+# 寫入檔案
+with open('products.csv', 'w') as f: 
     f.write('商品,價格\n')        
     for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')    # f.write 真正寫入   \n 換行符號
+        f.write(p[0] + ',' + p[1] + '\n') 
